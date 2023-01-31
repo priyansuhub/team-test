3beaf6720ecf111d98011f15fb70afae4e4107ed
priyansuhub <muniapriyansu@gmail.com>
2023-01-31 20:44:13 +0530

    requirements

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from .database import engine, get_db
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import  APIRouter, FastAPI,Response,status, HTTPException,Depends
 from .routers import student
 from .routers import question
@@ -9,6 +10,15 @@ from .routers import sem
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # app.include_router(question.router)
 app.include_router(student.router)
 app.include_router(auth.router)
